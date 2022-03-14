5c45085c9ce866b6b7a7881b11e72d1fd65b9fee
anonymous anonymous <mishmosh293@gmail.com>
2022-03-14 23:59:49 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "sqs#index"
   resources :sq_types
   resources :sqs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
